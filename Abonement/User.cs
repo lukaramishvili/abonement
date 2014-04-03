d7d6bbf093c7dde5e7a0b1b2dc7ef9da1550e25c
lukaramishvili <lukaramishvili@localhost>
2014-04-03 17:57:45 +0400

    everything working except filters and editing attendance time

---
+++ Abonement/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ManagementSystem
+{
+    class User
+    {
+        public string username;
+        public string password;
+
+        public User()
+        {
+        }
+
+        public User(string user, string pass)
+        {
+            this.username = user;
+            this.password = pass;
+        }
+    }
+}
